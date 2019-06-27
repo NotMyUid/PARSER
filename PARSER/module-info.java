00e5889da3ae90bc8bbf90cc6656d573e9bf9179
NotMyUid <giomigiu@gmail.com>
2019-06-27 21:52:01 +0200

    Bug fixes.

--- PARSER/module-info.java
+++
@@ -1,8 +0,0 @@
-module parser {
-	exports lab11_05_06.environments;
-	exports lab11_05_06.parser.ast;
-	exports lab11_05_06.parser;
-	exports lab11_05_06.visitors.typechecking;
-	exports lab11_05_06.visitors;
-	exports lab11_05_06.visitors.evaluation;
-}
