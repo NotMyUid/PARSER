001424ef6fc7225c06977d3c17342e2b66762718
RobbGar <49005244+RobbGar@users.noreply.github.com>
2019-08-28 15:05:37 +0200

    x

---
+++ src/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * @author Roberto
+ *
+ */
+module parser {
+	exports environments;
+	exports parser.ast;
+	exports parser;
+	exports visitors.typechecking;
+	exports visitors;
+	exports visitors.evaluation;
+}
